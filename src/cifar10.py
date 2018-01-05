5c51db6cfab3da77f46c565e92b0e7c89955e6c9
Leo Wan <jabberwockwanli@gmail.com>
2018-01-05 16:16:36 +0800

    cifar dataset

--- src/cifar10.py
+++ src/cifar10.py
@@ -11,8 +11,53 @@ File: cifar10.py
 Author: Wan Li
 Date: 2017/11/27 10:41:01
 """
+
+import os
 import tensorflow as tf
+import numpy as np
 import config
+import resnet
+
+def model_fn(features, labels, mode, params):
+    """Estimator API model function to build an custom tf.estimator.Estimator
+
+    1. Configure the model via TensorFlow operations
+    2. Define the loss function for training/evaluation
+    3. Define the training operation/optimizer
+    4. Generate predictions
+    5. Return predictions/loss/train_op/eval_metric_ops in EstimatorSpec object
+    detailed: https://www.tensorflow.org/extend/estimators
+
+    Returns:
+        tf.estimator.EstimatorSpec instance
+    """
+    # summaries
+    tf.summary.image('images', features, max_outputs=10)
+
+    # Input
+    X = tf.reshape(features, [-1, config.IMG_HEIGHT, config.IMG_WIDTH, config.IMG_DEPTH])
+
+    # graph
+    logits = resnet.build_graph(X, mode == tf.estimator.ModeKeys.TRAIN)
+
+    # predictions
+    predictions = {
+        'classes': tf.argmax(logits, axis=1),
+        'probabilities': tf.nn.softmax(logits, name='softmax_tensor')
+    }
+
+    # Mode: predict
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Mode: train or evaluate
+    loss = tf.losses.softmax_cross_entropy(logits=logits, onehot_labels=labels)
+    cross_entropy = tf.identity(loss, name='cross_entropy')
+    tf.summary.scalar('cross_entropy', cross_entropy)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # TODO
+        pass
 
 def input_fn(is_training):
     """Input function streaming train/predict data
@@ -21,9 +66,94 @@ def input_fn(is_training):
         is_training: True for get training data, False for prediction
 
     Returns:
-        images, labels
+        images, labels (generator object)
     """
-    pass
+    def data_file_paths(is_training):
+        """Get file names
+
+        Args:
+            is_training: True for get training data, False for prediction
+        Returns:
+            array of filenames
+        """
+        if is_training:
+            return [os.path.join(config.DATA_DIR, 'cifar-10-batches-bin/data_batch_%d.bin' % (i + 1)) \
+                    for i in xrange(5)]
+        else:
+            return [os.path.join(config.DATA_DIR, 'cifar-10-batches-bin/test_batch.bin')]
+
+    data_row_length = config.IMG_HEIGHT * config.IMG_WIDTH * config.IMG_DEPTH + 1 # 1 is the label byte
+    # dataset instance
+    dataset = tf.data.FixedLengthRecordDataset(
+        data_file_paths(is_training),
+        data_row_length
+    )
+
+    if is_training:
+        # shuffle dataset, prepare number of total training images
+        dataset = dataset.shuffle(buffer_size=config.TRAIN_IMG_NUM)
+
+    def parse_row(row_data):
+        """Parse data row to label, image
+
+        Args:
+            row_data: string representation of one row of data
+
+        Returns:
+            image, label tensor
+        """
+        pixel_num = config.IMG_DEPTH * config.IMG_HEIGHT * config.IMG_WIDTH
+        row_vec = tf.decode_raw(row_data, tf.uint8)
+        label = tf.one_hot(tf.cast(row_vec[0], tf.int32), config.CLASS_NUM)
+        img_chw = tf.reshape(row_vec[1:pixel_num + 1], [config.IMG_DEPTH, config.IMG_HEIGHT, config.IMG_WIDTH])
+        # convert to shape [h, w, c]
+        image = tf.cast(tf.transpose(img_chw, [1, 2, 0]), tf.float32)
+        return image, label
+
+    # parse to tensor, label pairs
+    dataset = dataset.map(parse_row)
+
+    def preprocess_image(image, is_training):
+        """Preprocess image
+
+        Randomly crop or affine transform if training
+        """
+        if is_training:
+            image = tf.image.resize_image_with_crop_or_pad(
+                image,
+                config.IMG_HEIGHT + 8,
+                config.IMG_WIDTH + 8
+            )
+            image = tf.random_crop(
+                image,
+                [config.IMG_HEIGHT, config.IMG_WIDTH, config.IMG_DEPTH]
+            )
+            image = tf.image.random_flip_left_right(image)
+        image = tf.image.per_image_standardization(image)
+        return image
+
+    # preprocess image
+    dataset = dataset.map(
+        lambda image, label: (preprocess_image(image, is_training), label)
+    )
+
+    # batch config
+    dataset.prefetch(config.BATCH_SIZE * 2)
+    dataset.repeat(1)
+    dataset = dataset.batch(config.BATCH_SIZE)
+    iterator = dataset.make_one_shot_iterator()
+    # data generator
+    images, labels = iterator.get_next()
+    return images, labels
+
 
 if __name__ == "__main__":
+    with tf.Session() as sess:
+        X, Y = input_fn(True)
+        for i in xrange(2):
+            print "== iter [%d]" % i
+            print "Y:", tf.shape(Y)
+            print "Y:", sess.run(Y)
+            #print "X:", tf.shape(X)
+            #print "X:", sess.run(X)
     pass
--- src/resnet.py
+++ src/resnet.py
@@ -16,143 +16,6 @@ import math
 import tensorflow as tf
 import config
 
-def apply_batchnorm_and_relu(layer, is_training):
-    """Apply batchnorm for resnet v2(K. He 2016) followed by ReLu.
-
-    Args:
-        layer: input layer
-        is_training: flag for moving average acculumator
-    Returns:
-        output layer
-    """
-    layer = tf.layers.batch_normalization(
-        inputs=layer,
-        axis=3,
-        training=is_training, # moving average mode
-        momentum=0.997, # moving average param
-        epsilon=1e-5, # moving average param
-        center=True, # add offset of beta
-        scale=True # multiply by gamma
-    )
-    layer = tf.nn.relu(layer)
-    return layer
-
-def apply_conv2d_with_padding(layer, filter_num, kernel_size, stride):
-    """Apply 2D convolution with paddings.
-
-    Args:
-        layer: input layer
-        filter_num: Integer, the dimensionality of the output space (i.e. the number of filters in the convolution).
-        kernel_size: An integer or tuple/list of 2 integers, specifying the height and width of the 2D convolution window. Can be a single integer to specify the same value for all spatial dimensions.
-        stride: An integer, specifying the strides of the convolution along the height and width. A single integer to specify the same value for all spatial dimensions.
-    Returns:
-        output layer
-    """
-    # paddings
-    if stride > 1:
-        # constant stride of 0 if stride > 1
-        # SAME padding if stride == 1
-        pad_total = kernel_size - 1
-        pad_head = int(math.floor(pad_total / 2))
-        pad_tail = pad_total - pad_head
-        layer = tf.pad(layer, [[0, 0], [pad_head, pad_tail], [pad_head, pad_tail], [0, 0]])
-
-    # conv2d
-    layer = tf.layers.conv2d(
-        inputs=layer,
-        filters=filter_num,
-        kernel_size=kernel_size,
-        strides=stride,
-        padding=("SAME" if stride == 1 else "VALID"),
-        use_bias=False
-    )
-    return layer
-
-def add_standard_building_block(layer, shortcut_fn, filter_num, stride, is_training):
-    """Standard building block for shallow ResNet (e.g. 34)
-
-    Args:
-        layer: input layer
-        shortcut_fn: tranform function applied for shortcut
-        filter_num: the number of filters in the convolution
-        stride: stride of the convolution along the height and width
-        is_training: flag for moving average acculumator
-    Returns:
-        output layer
-    """
-    shortcut_connection = layer
-    layer = apply_batchnorm_and_relu(layer, is_training)
-    if shortcut_fn is not None:
-        shortcut_connection = shortcut_fn(layer)
-    layer = apply_conv2d_with_padding(layer, filter_num, 3, stride)
-    layer = apply_batchnorm_and_relu(layer, is_training)
-    layer = apply_conv2d_with_padding(layer, filter_num, 3, 1)
-    layer = layer + shortcut_connection
-    return layer
-
-def add_bottleneck_building_block(layer, shortcut_fn, filter_num, stride, is_training):
-    """Bottleneck building block for deeper ResNet (e.g. 50/101/152/..)
-
-    Args:
-        layer: input layer
-        shortcut_fn: tranform function applied for shortcut
-        filter_num: the number of filters in the convolution
-        stride: stride of the convolution along the height and width
-        is_training: flag for moving average acculumator
-    Returns:
-        output layer
-    """
-    shortcut_connection = layer
-    layer = apply_batchnorm_and_relu(layer, is_training)
-    if shortcut_fn is not None:
-        shortcut_connection = shortcut_fn(layer)
-    layer = apply_conv2d_with_padding(layer, filter_num, 1, 1)
-    layer = apply_batchnorm_and_relu(layer, is_training)
-    layer = apply_conv2d_with_padding(layer, filter_num, 3, stride)
-    layer = apply_batchnorm_and_relu(layer, is_training)
-    # bottleneck building block has 4*original_filter_number
-    layer = apply_conv2d_with_padding(layer, 4*filter_num, 1, 1)
-    layer = layer + shortcut_connection
-    return layer
-
-def add_layer_compound(layer, filter_num, building_block_fn, block_num, stride, is_training):
-    """Construct compound layer of building blocks
-
-    Compound layers differs in number of channels (filer_num) and layer width/height
-    Args:
-        layer: input layer
-        filter_num: number of output channels
-        building_block_fn: function used to apply building block
-        block_num: number of building blocks in this layer compound
-        stride: stride of the convolution along the height and width
-        is_training: flag for moving average acculumator
-    Returns:
-        output layer
-    """
-    def shortcut_projection_fn(layer):
-        """Projection tranform from input to output size
-        Args:
-            layer: input layer
-        Returns:
-            output layer
-        """
-        if building_block_fn is add_bottleneck_building_block:
-            # bottleneck building block has 4*original_filter_number
-            layer = apply_conv2d_with_padding(layer, 4*filter_num, 1, stride)
-            return layer
-        else:
-            layer = apply_conv2d_with_padding(layer, filter_num, 1, stride)
-            return layer
-
-    # first building block do the width/height/depth projection
-    layer = building_block_fn(layer, shortcut_projection_fn, filter_num, stride, is_training)
-
-    # other building blocks apply stride = 1 and identity shortcut
-    for _ in xrange(1, block_num):
-        layer = building_block_fn(layer, None, filter_num, 1, is_training)
-
-    return layer
-
 def build_graph(x, is_training):
     """Build compute graph
 
@@ -220,6 +83,148 @@ def build_graph(x, is_training):
         units=config.CLASS_NUM
     )
     return layer
+
+
+def apply_batchnorm_and_relu(layer, is_training):
+    """Apply batchnorm for resnet v2(K. He 2016) followed by ReLu.
+
+    Args:
+        layer: input layer
+        is_training: flag for moving average acculumator
+    Returns:
+        output layer
+    """
+    layer = tf.layers.batch_normalization(
+        inputs=layer,
+        axis=3,
+        training=is_training, # moving average mode
+        momentum=0.997, # moving average param
+        epsilon=1e-5, # moving average param
+        center=True, # add offset of beta
+        scale=True # multiply by gamma
+    )
+    layer = tf.nn.relu(layer)
+    return layer
+
+
+def apply_conv2d_with_padding(layer, filter_num, kernel_size, stride):
+    """Apply 2D convolution with paddings.
+
+    Args:
+        layer: input layer
+        filter_num: Integer, the dimensionality of the output space (i.e. the number of filters in the convolution).
+        kernel_size: An integer or tuple/list of 2 integers, specifying the height and width of the 2D convolution window. Can be a single integer to specify the same value for all spatial dimensions.
+        stride: An integer, specifying the strides of the convolution along the height and width. A single integer to specify the same value for all spatial dimensions.
+    Returns:
+        output layer
+    """
+    # paddings
+    if stride > 1:
+        # constant stride of 0 if stride > 1
+        # SAME padding if stride == 1
+        pad_total = kernel_size - 1
+        pad_head = int(math.floor(pad_total / 2))
+        pad_tail = pad_total - pad_head
+        layer = tf.pad(layer, [[0, 0], [pad_head, pad_tail], [pad_head, pad_tail], [0, 0]])
+
+    # conv2d
+    layer = tf.layers.conv2d(
+        inputs=layer,
+        filters=filter_num,
+        kernel_size=kernel_size,
+        strides=stride,
+        padding=("SAME" if stride == 1 else "VALID"),
+        use_bias=False
+    )
+    return layer
+
+
+def add_standard_building_block(layer, shortcut_fn, filter_num, stride, is_training):
+    """Standard building block for shallow ResNet (e.g. 34)
+
+    Args:
+        layer: input layer
+        shortcut_fn: tranform function applied for shortcut
+        filter_num: the number of filters in the convolution
+        stride: stride of the convolution along the height and width
+        is_training: flag for moving average acculumator
+    Returns:
+        output layer
+    """
+    shortcut_connection = layer
+    layer = apply_batchnorm_and_relu(layer, is_training)
+    if shortcut_fn is not None:
+        shortcut_connection = shortcut_fn(layer)
+    layer = apply_conv2d_with_padding(layer, filter_num, 3, stride)
+    layer = apply_batchnorm_and_relu(layer, is_training)
+    layer = apply_conv2d_with_padding(layer, filter_num, 3, 1)
+    layer = layer + shortcut_connection
+    return layer
+
+
+def add_bottleneck_building_block(layer, shortcut_fn, filter_num, stride, is_training):
+    """Bottleneck building block for deeper ResNet (e.g. 50/101/152/..)
+
+    Args:
+        layer: input layer
+        shortcut_fn: tranform function applied for shortcut
+        filter_num: the number of filters in the convolution
+        stride: stride of the convolution along the height and width
+        is_training: flag for moving average acculumator
+    Returns:
+        output layer
+    """
+    shortcut_connection = layer
+    layer = apply_batchnorm_and_relu(layer, is_training)
+    if shortcut_fn is not None:
+        shortcut_connection = shortcut_fn(layer)
+    layer = apply_conv2d_with_padding(layer, filter_num, 1, 1)
+    layer = apply_batchnorm_and_relu(layer, is_training)
+    layer = apply_conv2d_with_padding(layer, filter_num, 3, stride)
+    layer = apply_batchnorm_and_relu(layer, is_training)
+    # bottleneck building block has 4*original_filter_number
+    layer = apply_conv2d_with_padding(layer, 4*filter_num, 1, 1)
+    layer = layer + shortcut_connection
+    return layer
+
+
+def add_layer_compound(layer, filter_num, building_block_fn, block_num, stride, is_training):
+    """Construct compound layer of building blocks
+
+    Compound layers differs in number of channels (filer_num) and layer width/height
+    Args:
+        layer: input layer
+        filter_num: number of output channels
+        building_block_fn: function used to apply building block
+        block_num: number of building blocks in this layer compound
+        stride: stride of the convolution along the height and width
+        is_training: flag for moving average acculumator
+    Returns:
+        output layer
+    """
+    def shortcut_projection_fn(layer):
+        """Projection tranform from input to output size
+        Args:
+            layer: input layer
+        Returns:
+            output layer
+        """
+        if building_block_fn is add_bottleneck_building_block:
+            # bottleneck building block has 4*original_filter_number
+            layer = apply_conv2d_with_padding(layer, 4*filter_num, 1, stride)
+            return layer
+        else:
+            layer = apply_conv2d_with_padding(layer, filter_num, 1, stride)
+            return layer
+
+    # first building block do the width/height/depth projection
+    layer = building_block_fn(layer, shortcut_projection_fn, filter_num, stride, is_training)
+
+    # other building blocks apply stride = 1 and identity shortcut
+    for _ in xrange(1, block_num):
+        layer = building_block_fn(layer, None, filter_num, 1, is_training)
+    return layer
+
 
 if __name__ == "__main__":
     with tf.Session() as sess:
